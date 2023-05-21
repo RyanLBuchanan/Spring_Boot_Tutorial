03b48ddccbc3f158a5adce44241d13f958fc484d
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-05-21 15:40:52 -0600

    feat: Added simple method

--- demo/src/main/java/com/ryangineer/demo/web/HelloController.java
+++ demo/src/main/java/com/ryangineer/demo/web/HelloController.java
@@ -6,9 +6,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/greeting")
 public class HelloController {
 
-	@RequestMapping(value="/", method = RequestMethod.GET)
+	@RequestMapping(value="", method = RequestMethod.GET)
 	public String sayHello() {
 		return "<h1>Hello Kittens!</h1>";
 //		System.out.println("hello");
